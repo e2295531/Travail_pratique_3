94a3918f8fe8eafa47ef7a74d07b3c9df1a4078a
e2295531 <e2295531@cmaisonneuve.qc.ca>
2022-04-29 11:00:04 -0400

    ajout de style css pour le tableau

--- table-temps.php
+++ table-temps.php
@@ -5,7 +5,52 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    
+    <style>
+        body, html{
+            font-family: Verdana, Geneva, Tahoma, sans-serif;
+            font-size: 1em;
+            margin: 20px 30px 0px 30px;
+            width: auto;
+        }
+        h1{
+            font-weight: bold;
+            font-size: 1.3em;
+        }
+        h2{
+            font-variant: small-caps;
+            font-weight: bold;
+            font-size: 1.2em;
+        }
+        h3{
+            font-weight: bold;
+            font-size: 1.1em;
+        }
+        table{
+            border: 2px solid black;
+            border-collapse:collapse;
+            width:auto;
+        }
+        th{
+            border: 1px solid black;
+            text-align: center;
+            font-variant: small-caps;
+            font-size: 1.2em;
+            color: black;
+            width:150px;
+        }
+        tr{
+            border-collapse:collapse;
+            border: 2px solid black;
+        }
+        td{
+            text-align: center;
+            color: black;
+            font-size: 1em;
+            border-collapse:collapse;
+            width: auto;
+            border: 1px solid black;
+        }
+    </style>
 </head>
 <body>
 <table>
